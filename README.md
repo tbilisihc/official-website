b741822de85a2711b403c7d38b2c328cf8640a80
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-06-01 21:55:41 +0400

    Index page added :checkmark:

--- README.md
+++
@@ -1,1 +0,0 @@
-# Just some static pages
---
+++ app/src/app/layout.tsx
@@ -0,0 +1,72 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Tbilisi Hack Club",
+  description: "Non-profit organization for students in Tbilisi, Georgia",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <header>
+        
+          <nav className="flex justify-between items-center">
+            <div className="text-2xl font-bold">Tbilisi Hack Club</div>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/" className="text-white-700 hover:text-gray-700">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/news" className="text-white-700 hover:text-gray-700">
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link href="/about" className="text-white-700 hover:text-gray-700">
+                  About
+                </Link>
+              </li>
+            </ul>          
+            </nav>  
+            
+        </header>
+        {children}
+    <div>
+        
+    </div>
+        <footer className="mt-8 text-center text-gray-500">
+          <p className="mt-4 text-lg">
+              Want to improve this site? Go to our{" "}
+              <a className="underline" href="https://github.com/orgs/tbilisihc">
+                github
+              </a>
+        </p>
+          <p>&copy; {new Date().getFullYear()} Tbilisi Hack Club</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
